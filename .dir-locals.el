f011e392528480ae5e476ada4e17f9bd1791d967
Maciej Barć <xgqt@gentoo.org>
2024-03-30 17:28:43 +0100

    .dir-locals.el: update rules
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,10 +3,16 @@
 ;; Directory Local Variables
 ;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((projectile-project-type . make)
-         (eval . (add-hook 'before-save-hook 'whitespace-cleanup))))
- (find-file . ((require-final-newline . t)
-               (show-trailing-whitespace . t)
-               (tab-width . 4)))
- (makefile-mode . ((indent-tabs-mode . t)))
- (yaml-mode . ((tab-width . 2))))
+((nil           . ((projectile-project-type . make)
+                   (eval . (progn
+                             (add-hook 'before-save-hook 'whitespace-cleanup)))))
+ (find-file     . ((require-final-newline . t)
+                   (show-trailing-whitespace . t)
+                   (tab-width . 4)))
+ (html-mode     . ((indent-tabs-mode . nil) (tab-width . 2)))
+ (js-json-mode  . ((indent-tabs-mode . nil) (tab-width . 2)))
+ (makefile-mode . ((indent-tabs-mode . t)   (tab-width . 4)))
+ (nxml-mode     . ((indent-tabs-mode . nil) (tab-width . 2)))
+ (python-mode   . ((indent-tabs-mode . nil) (tab-width . 4)))
+ (xml-mode      . ((indent-tabs-mode . nil) (tab-width . 2)))
+ (yaml-mode     . ((indent-tabs-mode . nil) (tab-width . 2))))
